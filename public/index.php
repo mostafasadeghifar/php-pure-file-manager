b4086e03bde39759d1fdb0e6a71977107f2ceb08
mostafasadeghifar <msadeghifar1996@gmail.com>
2022-03-05 23:06:22 +0330

    + add readme

--- public/index.php
+++ public/index.php
@@ -1,8 +1,5 @@
 <?php
 
-//run server => php -S 127.0.0.1:800 -t public
-
-// address => 1.absolute 2.relative
 define('ROOT_DIR',$_SERVER['DOCUMENT_ROOT'].'/../');
 include 'func/view.php';    //relative path
 include 'func/url/url.php';
@@ -13,10 +10,9 @@ $uri = $_SERVER['REQUEST_URI'];
 $urlComponent = parse_url($uri);
 
 $path = $urlComponent['path'];
-$query = $urlComponent['query']??null;  //todo: check null query
 
 /**
- * if uri just '/'
+ * if uri just '/' for main page
  */
 if ($path == '/') {
     include ROOT_DIR.'app/main.php';
--- view/file/create.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-?>
-
-<form action="<?= url('file/store') ?>" method="post">
-    <input type="text" name="name">
-    <input type="text" name="size">
-    <button type="submit">submit</button>
-</form>
