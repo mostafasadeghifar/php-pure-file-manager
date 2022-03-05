5f30d894c1c082fddb869d25ba158537d0201ed2
mostafasadeghifar <msadeghifar1996@gmail.com>
2022-03-05 22:22:15 +0330

    + added main page

--- view/main.php
+++ view/main.php
@@ -1,21 +1,29 @@
-<?php
-
-
-?>
-
-
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
 </head>
-<body>
-<h1>
-    design html for landing page
-</h1>
+<body style="height: 100vh;background-color: #e0b7b7"
+      class="d-flex flex-column align-items-center justify-content-center">
+
+
+<h1 class="h-full">This is sample php framework with php pure </h1>
+
+<h4> This app developed just for learn. usefull for beginner</h4>
+<h6><a href="https://github.com/mostafasadeghifar">by:mostafa sadeghifar</a> </h6>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
+
 </body>
 </html>
